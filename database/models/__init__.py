bf9cf5793be4b84bbfafe1022e5e05be12222657
ThanhPinkIT <tthanhhong1302@gmail.com>
2026-01-09 16:19:19 +0700

    Delete database directory

--- database/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .user import User
-from .project import Project
-from .project_member import ProjectMember
-from .task import Task
-from .tenant import Tenant
-from .tenant_user import TenantUser
